bfcdbeefe1a41fe27a7744a70996bd579e7a8ee8
Fabio Orozco <forozcog@cisco.com>
2026-02-27 01:21:58 -0500

    environment production=true

--- src/environment.dev.ts
+++ src/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: process.env['FIREBASE_API_KEY_DEV'],
     authDomain: process.env['FIREBASE_AUTH_DOMAIN_DEV'],
